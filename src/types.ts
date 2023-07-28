ef8e0c878edb8907acbdde0a29c8fbf478fc6329
Anton Lovkin <anton28111987@gmail.com>
2023-07-28 13:20:23 +0300

    added types file

---
+++ src/types.ts
@@ -0,0 +1,13 @@
+export interface NoteI {
+    id: string,
+    createdAt: string,
+    content: string,
+    category: string,
+    name: string,
+    dates: string,
+    isArchived: boolean,
+};
+
+export interface InitialState {
+  notes: NoteI[];
+}
