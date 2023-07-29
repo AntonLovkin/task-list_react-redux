d65d13a5e905e0bcf6f554f4957d5e8f7f30f179
Anton Lovkin <anton28111987@gmail.com>
2023-07-29 16:14:31 +0300

    deleted component

--- src/components/ArchivedNotes.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ArchivedNotes() {
-  return (
-    <div>ArchivedNotes</div>
-  )
-}
-
-export default ArchivedNotes
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,5 @@
 import Notes from "./Notes";
 import Note from "./Note";
-import ArchivedNotes from "./ArchivedNotes";
 import Summary from "./Summary";
 import Form from "./Form";
 import NotesHeader from "./NotesHeader";
@@ -9,7 +8,6 @@ import Icon from "./Icon";
 
 export {
     Notes,
-    ArchivedNotes,
     Summary,
     Form,
     Note,
