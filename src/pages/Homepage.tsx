12399cc3644565789ea7c2e042000ddeae519e79
Anton Lovkin <anton28111987@gmail.com>
2023-07-29 14:42:26 +0300

    cleaned code

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,24 +1,9 @@
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { NotesList, Summary, Form } from "../components";
-import { toggleShowForm } from '../actions/actions';
-import { InitialState } from '../types';
+import { Notes, Summary } from "../components";
 
 function Homepage() {
-
-  const [showAddForm, setShowAddForm] = useState(false)
-  const showForm = useSelector((state: InitialState) => state.showForm);
-  const dispatch = useDispatch();
-
-  const handleClick = () => {
-    dispatch(toggleShowForm())
-  };
-
   return (
     <>
-      <NotesList showForm={handleClick}/>
-      <button onClick={handleClick}>Add note</button>
-      {showForm && <Form note={null} setShowForm={setShowAddForm} />}
+      <Notes />
       <Summary />
     </>
   )
