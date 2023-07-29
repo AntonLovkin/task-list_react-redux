0f60334ecab6f9a98214e991bf4a914c9e0b4fd0
Anton Lovkin <anton28111987@gmail.com>
2023-07-29 15:50:43 +0300

    fixed components elements order render

--- src/components/Notes.tsx
+++ src/components/Notes.tsx
@@ -26,23 +26,39 @@ function Notes() {
     const handleClick = () => {
         dispatch(showFormByType(FORM_TYPES.ADD))
     };
-
+console.log(notes)
     return (<div>
+        <NotesHeader />
         <ul>
-            <NotesHeader />
-            {activeNotes && activeNotes.length > 0 && activeNotes.map(note => <Note note={note} editBtnClick={editBtnClick} />)}
+            {activeNotes
+                && activeNotes.length > 0
+                && activeNotes.map(note => <Note
+                    note={note}
+                    editBtnClick={editBtnClick}
+                    isArchived={false}
+                />)}
         </ul>
 
         <button onClick={handleClick}>Add note</button>
-        {showForm && type === FORM_TYPES.ADD && <Form note={null} />}
-        {showForm && type === FORM_TYPES.EDIT && <Form note={noteData} />}
+        {showForm
+            && type === FORM_TYPES.ADD
+            && <Form note={null} />}
+        {showForm
+            && type === FORM_TYPES.EDIT
+            && <Form note={noteData} />}
 
-        {archivedNotes && archivedNotes.length > 0 && <div>
-            <h2>Archived notes list</h2>
-            <ul>
-                {archivedNotes.map(note => <Note note={note} editBtnClick={editBtnClick} />)}
-            </ul>
-        </div>}
+        {archivedNotes
+            && archivedNotes.length > 0
+            && <div>
+                <h2>Archived notes list</h2>
+                <ul>
+                    {archivedNotes.map(note => <Note
+                        note={note}
+                        editBtnClick={editBtnClick}
+                        isArchived={true}
+                    />)}
+                </ul>
+            </div>}
     </div>);
 }
 
