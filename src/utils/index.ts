79a84c281f93ce42d0d9e821036b851daf9cc724
Anton Lovkin <anton28111987@gmail.com>
2023-07-29 14:40:20 +0300

    creared variables constants

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,13 @@
 import getCurrentDateTime from "./getCurrentDateTime";
 import getDatesFromNote from "./getDatesFromNote";
 import { separateAndCountByCategory } from "./separateAndCountByCategory";
+import { FORM_TYPES } from "./variables";
+import { ACTION_TYPES } from "./variables";
 
-export { getCurrentDateTime, getDatesFromNote, separateAndCountByCategory };
+export {
+    getCurrentDateTime,
+    getDatesFromNote,
+    separateAndCountByCategory,
+    FORM_TYPES,
+    ACTION_TYPES,
+};
