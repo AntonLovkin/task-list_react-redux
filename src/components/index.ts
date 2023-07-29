2cedb1d5945e2fe7f4a508ebaf30130c5272cf6e
Anton Lovkin <anton28111987@gmail.com>
2023-07-29 14:41:51 +0300

    delete component NotesList

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,3 @@
-import NotesList from "./NotesList";
 import Notes from "./Notes";
 import Note from "./Note";
 import ArchivedNotes from "./ArchivedNotes";
@@ -8,7 +7,6 @@ import NotesHeader from "./NotesHeader";
 import SummaryHeader from "./SummaryHeader";
 
 export {
-    NotesList,
     Notes,
     ArchivedNotes,
     Summary,
